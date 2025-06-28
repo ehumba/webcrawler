05301c552ab99b6a56b1599821c768af72c90e25
ehumba <behsukiat@gmail.com>
2025-06-28 12:45:06 +0800

    Made test cases for url_from_html

---
+++ url_from_html_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetURLFromHTML(t *testing.T) {
+	cases := []struct {
+		name      string
+		inputURL  string
+		inputBody string
+		expected  []string
+	}{
+		{name: "absolute and relative URLs",
+			inputURL: "https://blog.boot.dev",
+			inputBody: `
+<html>
+	<body>
+		<a href="/path/one">
+			<span>Boot.dev</span>
+		</a>
+		<a href="https://other.com/path/one">
+			<span>Boot.dev</span>
+		</a>
+	</body>
+</html>
+`,
+			expected: []string{"https://blog.boot.dev/path/one", "https://other.com/path/one"},
+		},
+		{
+			name:     "test #2",
+			inputURL: "https://developer.mozilla.org",
+			inputBody: `
+<html>
+	<body>
+		<a href="/en-US/docs/Web">
+			<span>mozilla.org</span>
+		</a>
+		<a href="https://other.com/path/one">
+			<span>mozilla.org</span>
+		</a>
+	</body>
+</html>
+`,
+		},
+	}
+
+	for i, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := getURLsFromHTML(tc.inputURL, tc.inputBody)
+			if err != nil {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			}
+			if reflect.DeepEqual(actual, tc.expected) == false {
+				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
+			}
+		})
+	}
+}
