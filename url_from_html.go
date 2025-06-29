5f299a0bcd775097b257b7b9ef28662eb6e4a618
ehumba <behsukiat@gmail.com>
2025-06-29 14:00:54 +0800

    Made url_from_html function

--- url_from_html.go
+++ url_from_html.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"errors"
+	"net/url"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
-	return nil, nil
+	// Need to read through the HTML body, add the path to the raw base url, and add it to the
+	// array, or add the whole url if it is an absolute url.
+	// Create an io.Reader with strings.NewReader(htmlBody). Then use this reader with html.Parse
+	// to create a tree of nodes. Navigate it and find the <a anchors (the Node.Data?)
+	// If it is an absolut URL (starts with https://), just add it to the slice as is, if not, append it to the rawBaseURL
+	// and add to the slice.
+	parsedBaseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, errors.New("could not parse base url")
+	}
+	reader := strings.NewReader(htmlBody)
+	tree, err := html.Parse(reader)
+	if err != nil {
+		return nil, errors.New("could not parse html body")
+	}
+	var newURLs []string
+	for node := range tree.Descendants() {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, att := range node.Attr {
+				if att.Key == "href" {
+					parsedURLRef, err := url.Parse(att.Val)
+					if err != nil {
+						return nil, errors.New("could not parse url reference")
+					}
+					finalURL := parsedBaseURL.ResolveReference(parsedURLRef)
+					newURLs = append(newURLs, finalURL.String())
+				}
+			}
+		}
+	}
+	return newURLs, nil
 }
--- url_from_html_test.go
+++ url_from_html_test.go
@@ -43,12 +43,13 @@ func TestGetURLFromHTML(t *testing.T) {
 	</body>
 </html>
 `,
+			expected: []string{"https://developer.mozilla.org/en-US/docs/Web", "https://other.com/path/one"},
 		},
 	}
 
 	for i, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			actual, err := getURLsFromHTML(tc.inputURL, tc.inputBody)
+			actual, err := getURLsFromHTML(tc.inputBody, tc.inputURL)
 			if err != nil {
 				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
 				return
